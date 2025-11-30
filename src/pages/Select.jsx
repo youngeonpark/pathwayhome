808a395df3532058562f33b84a6fef70820b9511
Young Eon Park <93845759+yepp0502@users.noreply.github.com>
2025-11-30 17:34:56 -0500

    select page

--- src/pages/Select.jsx
+++ src/pages/Select.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Routes, Route, Link } from 'react-router-dom';
 import navImg from '../assets/images/navigation.png';
 import './Select.css'
 
 function Select() {
-  return (
+    const [side, setSide] = useState("left");
+    const [query, setQuery] = useState("");
+
+    const counties = [
+        "Chautauqua County",
+        "Erie County",
+        "Livingston County",
+        "Orleans County",
+        "Wyoming County"
+    ];
+
+    const filteredCounties = counties.filter(c =>
+        c.toLowerCase().startsWith(query.toLowerCase())
+    );
+
+    return (
     <>
         <Routes>
             <Route
@@ -13,22 +28,64 @@ function Select() {
                     <>
                         <div className="selectpg">
                             <div className="select-container">
-                                <div className="regcount-selector"> 
-                                    <div className="selection"></div>
+                                <div className={`regcount-selector ${side}`}> 
+                                    <div className={`selection ${side}`}></div>
                                     <div className="base">
-                                        <span id="region">Region</span>
-                                        <span id="county">County</span>
+                                        <span id="region" onClick={() => setSide("left")}>Region</span>
+                                        <span id="county" onClick={() => setSide("right")}>County</span>
                                     </div> 
                                 </div>
-                                <label>
-                                    <input name="myInput" placeholder="Search your county" id="searchbox"/>
-                                </label>
-                                <div className="options">
-                                    <div>Chautauqua County</div>
-                                    <div>Chautauqua County</div>
-                                    <div>Chautauqua County</div>
-                                    <div>Chautauqua County</div>
-                                </div>
+                                {side === "left" && (
+                                    <div className="left-options">
+                                        <div className="option">
+                                            <div className="title">
+                                                <h3>Region 1</h3>
+                                                <p>Wende Hub</p>
+                                            </div>
+                                            <div id="nextsym">&gt;</div>
+                                        </div>
+                                        <div className="option">
+                                            <div className="title">
+                                                <h3>Region 2</h3>
+                                                <p>Clinton Hub, Watertown Hub</p>
+                                            </div>
+                                            <div id="nextsym">&gt;</div>
+                                        </div>
+                                        <div className="option">
+                                            <div className="title">
+                                                <h3>Region 3</h3>
+                                                <p>Elmira Hub, Oneida Hub</p>
+                                            </div>
+                                            <div id="nextsym">&gt;</div>
+                                        </div>
+                                        <div className="option">
+                                            <div className="title">
+                                                <h3>Region 4</h3>
+                                                <p>Great Meadow Hub</p>
+                                            </div>
+                                            <div id="nextsym">&gt;</div>
+                                        </div>
+                                        <div className="option">
+                                            <div className="title">
+                                                <h3>Region 5</h3>
+                                                <p>Great Haven Hub, New York City Hub</p>
+                                            </div>
+                                            <div id="nextsym">&gt;</div>
+                                        </div>
+                                    </div>
+                                )}
+                                {side === "right" && (
+                                    <>
+                                        <label>
+                                            <input name="myInput" placeholder="Search your county" value={query} onChange={(e) => setQuery(e.target.value)} id="searchbox"/>
+                                        </label>
+                                        <div className="right-options">
+                                            {filteredCounties.map((c, idx) => (
+                                                <div key={idx}>{c}</div>
+                                            ))}
+                                        </div>
+                                    </>
+                                )}
                             </div>
                             <div className="main-container">
                                 <img src={navImg} alt="icon" id="nav-icon" />
@@ -41,7 +98,6 @@ function Select() {
                 }
             />
         </Routes>
-        
     </>
   )
 }
