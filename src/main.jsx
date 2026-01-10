0d77ad8157cd2bd2dd22c773a13ef5cb6d3d97c9
Young Eon Park <93845759+yepp0502@users.noreply.github.com>
2026-01-10 11:50:20 -0500

    configure vite and main

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './pages/App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename='/pathwayhome'>
     <App />
   </BrowserRouter>
 )
